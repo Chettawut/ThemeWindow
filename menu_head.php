959f8875e8315db75568562427da23c48abd4bea
Chayapat <papae_peez02@hotmail.co.th>
2023-02-03 15:57:29 +0700

    first

---
+++ menu_head.php
@@ -0,0 +1,60 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+
+    <ul class="navbar-nav" id="TopDashboard" style="display:none;">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="<?php echo PATH; ?>" role="button">
+                <i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-sm-inline-block">
+            <a href="<?php echo PATH; ?>/store/rr" class="nav-link">Dashboard</a>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav" id="TopStore" style="display:none;">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="<?php echo PATH; ?>" role="button">
+                <i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-sm-inline-block">
+            <a href="<?php echo PATH; ?>/store/rr" class="nav-link">GoodsReceipt</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+    </ul>
+    
+    <ul class="navbar-nav" id="TopPurchase" style="display:none;">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="<?php echo PATH; ?>" role="button">
+                <i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-sm-inline-block">
+            <a href="<?php echo PATH; ?>/store/rr" class="nav-link">Goods Receipt ใบรับสินค้า</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item d-sm-inline-block">
+            <a href="#" class="nav-link">Logout</a>
+        </li>
+    </ul>
+</nav>
